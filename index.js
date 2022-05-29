a64c0668dc151abc2190effd44091e871db80ca3
Adarsh <91724820+AA8528@users.noreply.github.com>
2022-05-29 17:01:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let x= 5
+let y= prompt("value")
+
+function firstFunction(){
+    let z = y%x
+    if (z==0){
+        alert("It is divisible")
+    }else{
+        alert("It is not divisible")
+    }
+}
+
+firstFunction()
